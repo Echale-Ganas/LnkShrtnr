32192efa683379f1bd18947a41e264c114aeb314
buenobunny <alexparra@berkeley.edu>
2023-11-17 13:07:13 -0800

    Scaffolding for frontend

---
+++ pages/createLink.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreateLink() {
+    return <p>lol</p>;
+}
+
+export default CreateLink;
--- src/frontend.tsx
+++ src/frontend.tsx
@@ -1,5 +1,8 @@
 import {renderToReadableStream} from "react-dom/server";
 import Home from "../pages/home";
+import Login from "../pages/login";
+import Admin from "../pages/admin";
+import CreateLink from "../pages/createLink";
 
 function HandleFrontend(req: Request, resolve, reject): Promise<Response> {
     const url = new URL(req.url);
@@ -9,7 +12,21 @@ function HandleFrontend(req: Request, resolve, reject): Promise<Response> {
             resolve(new Response(stream, {
                 headers: { "Content-Type": "text/html" },
             }));
-            console.log("resolved")
+        } else if (url.pathname === "/login") {
+            const stream = await renderToReadableStream(<Login/>);
+            resolve(new Response(stream, {
+                headers: { "Content-Type": "text/html" },
+            }));
+        } else if (url.pathname === "/admin") {
+            const stream = await renderToReadableStream(<Admin/>);
+            resolve(new Response(stream, {
+                headers: { "Content-Type": "text/html" },
+            }));
+        } else if (url.pathname === "/create") {
+            const stream = await renderToReadableStream(<CreateLink/>);
+            resolve(new Response(stream, {
+                headers: { "Content-Type": "text/html" },
+            }));
         }
     });
 }
